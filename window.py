c6f69fe78f8966395764d38e1e5fcfafd1e116e5
mozhaaa <mozhay2005@gmail.com>
2022-10-04 23:54:39 +0300

    color added. now you can set color when add/addlist or edit color, also copy and cut saves color. now selected is not fills with red, but outline becomes red

--- window.py
+++ window.py
@@ -112,18 +112,33 @@ class Window(tk.Tk):
             if det.name not in self.colors:
                 self.colors[det.name] = "#CDCDCD"# random_color()
             if self.selected != None and self.selected.name == det.name:
-                color = "#F07777"
+                # color = "#F07777"
+                color = self.colors[det.name]
                 textcolor = "#FF0000"
             else:
                 color = self.colors[det.name]
                 textcolor = 'black'
-            self.create_rectangle(x1, y1, x2, y2, fill=color, alpha=.3)
+            self.create_rectangle(x1, y1, x2, y2, fill=color, outline=textcolor, alpha=.3)
             self.canvas.create_text((x1 + x2) / 2, y1 - 6, text=det.name, fill=textcolor, font=('Helvetica 12'))
 
     def button_click(self):
         self.run_query(self.entry_text.get().split(" "))
         self.entry_text.set("")
         self.show_details()
+
+    def parse_color(string):
+        if string[0] == '#' and len(string) == 7:
+            return string
+        elif string[:3] == 'rgb':
+            hx = '#' + ''.join(map(lambda x: hex(int(x))[2:].zfill(2), string[4:-1].split(',')))
+            if len(hx) != 7:
+                print(hx)
+                print("Invalid rgb values")
+                return False
+            return hx
+        else:
+            print("Invalid color option")
+            return False
 
     def run_query(self, query):
         try:
@@ -133,6 +148,10 @@ class Window(tk.Tk):
 
                 case "add":
                     self.details.append(Detail(name=query[1], mass=float(query[2]), length=float(query[3])))
+                    if len(query) > 4:
+                        color = Window.parse_color(query[4])
+                        if color != False:
+                            self.colors[query[1]] = color
                     print("Added new detail: ", repr(self.details[-1]))
 
                 case "move":
@@ -199,6 +218,10 @@ class Window(tk.Tk):
                                 obj.mass = float(query[3])
                             if query[2] == "length":
                                 obj.length = float(query[3])
+                            if query[2] == "color":
+                                color = Window.parse_color(query[3])
+                                if color != False:
+                                    self.colors[obj.name] = color
                             break
                     else:
                         print("No detail with this name")
@@ -222,7 +245,12 @@ class Window(tk.Tk):
                         listed_details = json.load(file)
                     for det in listed_details:
                         if det["name"].lower() == query[1].lower():
-                            self.run_query(("add", query[2], det["mass"], det["length"]))
+                            if len(query) > 3:
+                                color = Window.parse_color(query[3])
+                                if color != False:
+                                    self.run_query(("add", query[2], det["mass" ], det["length"], color))
+                            else:
+                                self.run_query(("add", query[2], det["mass"], det["length"]))
                             break
                     else:
                         print("No detail in listed file with this name")
@@ -243,8 +271,8 @@ class Window(tk.Tk):
                                 name1, name2 = query[3], query[4]
                             else:
                                 name1, name2 = det.name + '1', det.name + '2'
-                            self.run_query(("add", name1, det.mass*ratioa, det.length*ratioa))
-                            self.run_query(("add", name2, det.mass*ratiob, det.length*ratiob))
+                            self.run_query(("add", name1, det.mass*ratioa, det.length*ratioa, self.colors[det.name]))
+                            self.run_query(("add", name2, det.mass*ratiob, det.length*ratiob, self.colors[det.name]))
                             self.run_query(("move_to", name1, det.pos))
                             self.run_query(("move_to", name2, det.pos + det.length*ratioa))
                             self.run_query(("remove", det.name))
@@ -255,7 +283,7 @@ class Window(tk.Tk):
                 case "copy":
                     for det in self.details:
                         if det.name == query[1]:
-                            self.run_query(("add", query[2], det.mass, det.length))
+                            self.run_query(("add", query[2], det.mass, det.length, self.colors[det.name]))
                             self.run_query(("move_to", query[2], det.pos))
                             break
                     else:
